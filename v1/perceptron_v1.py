57c003e8ac3dc3f695c4a5262518fca08683c62d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-01-07 12:55:28 -0600

    primera version del perceptrón funcionando

---
+++ v1/perceptron_v1.py
@@ -0,0 +1,75 @@
+import csv
+
+
+# cargar el archivo CSV con los datos de entrenamiento como diccionario
+with open("../data/penguins.csv") as csvfile:
+    data = list(csv.DictReader(csvfile))
+
+# Limpiando los datos
+data = [
+    row
+    for row in data
+    if row["bill_depth_mm"] != "NA" and row["flipper_length_mm"] != "NA"
+]
+
+
+for row in data:
+    row["bill_depth_mm"] = float(row["bill_depth_mm"])
+    row["flipper_length_mm"] = float(row["flipper_length_mm"])
+
+
+def paso(x):
+    """
+    Convierte cualquier número a 0 o 1
+    """
+    if x < 0:
+        return 0
+    else:
+        return 1
+
+
+def clasificar(x, w1, w2, b):
+    """Recibe una fila de datos y devuelve 1 si es Gentoo y 0 si no lo es"""
+    return paso(w1 * x["bill_depth_mm"] + w2 * x["flipper_length_mm"] + b)
+
+
+def entrenar(datos, iteraciones):
+    # inicializamos los parámetros, esto puede ser aleatorio o cero, como lo hacemos aquí
+    w1 = w2 = b = 0
+    while iteraciones > 0:
+        iteraciones -= 1
+        for x in datos:
+            etiqueta_real = int(x["species"] == "Gentoo")
+            clase = clasificar(x, w1, w2, b)
+
+            if etiqueta_real == 1 and clase == 0:
+                # Aquí tenemos un Gentoo mal clasificado, tenemos que
+                # aumentar w1 y w2 para que la función lineal se acerque
+                # a la etiqueta real
+                w1 += x["bill_depth_mm"]
+                w2 += x["flipper_length_mm"]
+                b += 1  # Valor escogido arbitrariamente
+            elif etiqueta_real == 0 and clase == 1:
+                # Aquí tenemos un NO Gentoo mal clasificado, tenemos que
+                # disminuir w1 y w2 para que la función lineal se acerque
+                # a la etiqueta real
+                w1 -= x["bill_depth_mm"]
+                w2 -= x["flipper_length_mm"]
+                b -= 1  # valor escogido arbitrariamente
+        print("Iteración", iteraciones, "w1:", w1, "w2:", w2, "b:", b)
+    return w1, w2, b
+
+
+w1, w2, b = entrenar(data, 10000)
+
+for x in data:
+    clase = clasificar(x, w1, w2, b)
+    etiqueta_real = int(x["species"] == "Gentoo")
+    if clase == etiqueta_real:
+        print(x["species"], clase)
+        print("Etiquetado Correctamente")
+    else:
+        if clase == 1:
+            print("Es NO Gentoo etiquetado Incorrectamente")
+        else:
+            print("Es Gentoo etiquetado Incorrectamente")
